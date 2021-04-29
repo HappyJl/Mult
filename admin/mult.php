ef85f7523c00534713b75ed385e84333cf268f64
HappyJl <68591223+HappyJl@users.noreply.github.com>
2021-04-29 17:02:14 +0300

    0.1.2

--- admin/mult.php
+++ admin/mult.php
@@ -9,30 +9,44 @@
     <link rel="stylesheet" href="admin_style/mult_style.css">
 </head>
 <body>
+
 <?php
     include '../php/db.php';
+    $mult = mysqli_query($connect,"SELECT * FROM `cartoon`");
+    while ($mult_res = mysqli_fetch_assoc($mult)){
+        $stran = mysqli_query($connect,"SELECT * FROM `country` WHERE  `id страны` =".$mult_res['Страна производитель']);
+        $stran_res = mysqli_fetch_assoc($stran);
+        ?>
+        <table>
+            <tr>
+                <th>Id мультфильма</th>
+                <th>Название</th>
+                <th>Количество эпизодов</th>
+                <th>Продолжительность эпизода</th>
+                <th>Возрастной рейтинг</th>
+                <th>Год выпуска</th>
+                <th>Страна производитель</th>
+                <th>Описание</th>
+                <th>Рейтинг мультфильма</th>
+                <th>Изображение</th>
+                <th>Студия</th>
+            </tr>
+            <tr>
+                <td><?php echo $mult_res['Id мультфильма']?></td>
+                <td><?php echo $mult_res['Название']?></td>
+                <td><?php echo $mult_res['Количество эпизодов']?></td>
+                <td><?php echo $mult_res['Прод. эпиз.']?></td>
+                <td><?php echo $mult_res['Возрастной рейтинг']?></td>
+                <td><?php echo $mult_res['Год выпуска']?></td>
+                <td><?php echo $stran_res['Страна']?></td>
+                <td><?php echo $mult_res['Описание']?></td>
+                <td><?php echo $mult_res['Рейтинг мультфильма']?></td>
+                <td><img class="img_table" src="../img/mult/<?php echo $mult_res['Изображение']?>" alt=""></td>
+                <td><?php echo $mult_res['id студии']?></td>
+            </tr>
+        </table>
+<?php
+    }
 ?>
-<table>
-    <tr>
-        <th>Id мультфильма</th>
-        <th>Название</th>
-        <th>Количество эпизодов</th>
-        <th>Продолжительность эпизода</th>
-        <th>Возрастной рейтинг</th>
-        <th>Год выпуска</th>
-        <th>Страна производитель</th>
-        <th>Рейтинг мультфильма</th>
-        <th>Изображение</th>
-        <th>Студия</th>
-    </tr>
-    <tr>
-        <td>3</td>
-        <td>4</td>
-    </tr>
-    <tr>
-        <td>5</td>
-        <td>6</td>
-    </tr>
-</table>
 </body>
 </html>
